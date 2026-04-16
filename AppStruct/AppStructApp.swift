34ba9c56ad6a04ed4d05074896a8731c5ec5b334
Man <minhman000002@gmail.com>
2026-04-16 11:25:23 +0700

    Initial Commit

---
+++ AppStruct/AppStructApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStructApp.swift
+//  AppStruct
+//
+//  Created by Man Huynh on 16/4/26.
+//
+
+import SwiftUI
+
+@main
+struct AppStructApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStruct/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppStruct
+//
+//  Created by Man Huynh on 16/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
